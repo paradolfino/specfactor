a1f409f06d4978411cf1b930fea20f1f6baa31cb
viktharien <viktharien@zoho.com>
2018-08-07 17:34:09 -0400

    add singularize method to utils

--- lib/factory/spec_module.rb
+++ lib/factory/spec_module.rb
@@ -5,19 +5,24 @@ module Utils
   def self.pluralize(string)
     return ActiveSupport::Inflector.pluralize(string)
   end
+
+  def self.singularize(string)
+    return ActiveSupport::Inflector.singularize(string)
+  end
 end
 module SpecModule
   include Utils
+
   def self.index(name)
     "describe 'GET #index' do
       it 'returns http success' do
         get :index
         expect(response).to have_http_status(:success)
       end
-      it 'assigns @#{Utils.pluralize(name)} to #{name.capitalize}.all' do
-        #{name} = create(:#{name})
+      it 'assigns @#{Utils.pluralize(name)} to #{Utils.singularize(name.capitalize)}.all' do
+        #{Utils.singularize(name)} = create(:#{Utils.singularize(name)})
         get :index
-        expect(assigns(:#{Utils.pluralize(name)})).to eq([#{name}])
+        expect(assigns(:#{Utils.pluralize(name)})).to eq([#{Utils.singularize(name)}])
       end
     end"
   end
@@ -25,15 +30,15 @@ module SpecModule
   def self.show(name)
     "describe 'GET #show' do
       it 'returns http success' do
-        #{name} = create(:#{name})
-        get :show, params: {id: #{name}.to_param}
+        #{Utils.singularize(name)} = create(:#{Utils.singularize(name)})
+        get :show, params: {id: #{Utils.singularize(name)}.to_param}
         expect(response).to have_http_status(:success)
       end
 
-      it 'assigns @#{name} to a #{name.capitalize}' do
-        #{name} = create(:#{name})
-        get :show, params: {id: #{name}.to_param}
-        expect(assigns(:#{name})).to eq(#{name})
+      it 'assigns @#{Utils.singularize(name)} to a #{Utils.singularize(name.capitalize)}' do
+        #{Utils.singularize(name)} = create(:#{Utils.singularize(name)})
+        get :show, params: {id: #{Utils.singularize(name)}.to_param}
+        expect(assigns(:#{Utils.singularize(name)})).to eq(#{Utils.singularize(name)})
       end
     end"
   end
